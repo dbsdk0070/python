45f543e3251f86d8f1ff43ea0829d2e5f7e11cdd
dbsdk0070 <dbsdk0070@naver.com>
2021-04-05 11:49:26 +0900

    message1

--- web1.py
+++ web1.py
@@ -11,4 +11,21 @@ soup = BeautifulSoup(page,"html.parser")
 #<p>를 모두 검색해~~ 리스트 형식으로 리턴
 #print(soup.find_all("p"))
 #첫번째 <p>를 검색
-print(soup.find("p"))
+#print(soup.find("p"))
+#<p class="outer-text">된 경우 (약간의 필터링)
+#print(soup.find_all)("p", class_="outer-text"))
+#print(soup.find(id="first"))
+#print(soup.find_all("a"))
+#print(soup.find_all("b"))
+
+#태그를 제거하고 내부 문자열(컨텐츠만)
+#<p>컨텐츠</p>
+for tag in soup.find_all("p"):
+    #앞뒤에 공백문자 제거
+    print(tag.text.strip())
+    
+
+
+
+
+    
