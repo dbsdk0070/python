3445b876273490b273f1a7c72cbd17bdb487530e
NamyoonA <dbsdk0070@naver.com>
2021-04-07 12:31:50 +0900

    DB두번째

--- DemoDB1.py
+++ DemoDB1.py
@@ -10,6 +10,16 @@ cur = con.cursor()
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick', '010-111');")
+
+#입력 파라메터 처리
+#텍스트박스(GUI, Web Page)에서 입력을 받아서 처리
+name = "gildong"
+phoneNumber = "010-222"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber))
+#다중의 레코드(행, row)를 입력받는 경우: 2차원 행열데이터(0행의 0열, 0행의 1열...)
+datalist = (("tom", "010-123"),("dsp", "010-567")) #list안에 list가 들어가있음
+cur.executemany("insert into PhoneBook values (?, ?);", datalist)
+
 #검색
 cur.execute("select * from PhoneBook;") # select * : *는 모든 컬럼 몽땅 가져와라
 for row in cur:
